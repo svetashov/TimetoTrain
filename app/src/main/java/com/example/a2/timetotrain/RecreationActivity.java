2f68cc62bc9b6c1825f56a7cce3c09d0e7147788
Artyom Svetashov <arsvetashov@gmail.com>
2017-05-12 13:29:12 +0300

    Activity added changes to Recreation Activity

--- app/src/main/java/com/example/a2/timetotrain/RecreationActivity.java
+++ app/src/main/java/com/example/a2/timetotrain/RecreationActivity.java
@@ -1,11 +1,8 @@
 package com.example.a2.timetotrain;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 public class RecreationActivity extends AppCompatActivity {
 
@@ -13,17 +10,7 @@ public class RecreationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recreation);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
