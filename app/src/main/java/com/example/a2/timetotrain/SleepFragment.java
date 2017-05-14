99966ddac798e3847f5e36a334c59ec13af435a4
Artyom Svetashov <arsvetashov@gmail.com>
2017-05-14 20:50:06 +0300

    chaged SleepFragment

--- app/src/main/java/com/example/a2/timetotrain/SleepFragment.java
+++ app/src/main/java/com/example/a2/timetotrain/SleepFragment.java
@@ -6,15 +6,21 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import com.jjoe64.graphview.GraphView;
 
 
 public class SleepFragment extends Fragment {
 
+    private GraphView graphViewSleep;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sleep, container, false);
+
+        View currentView = inflater.inflate(R.layout.fragment_sleep, container, false);
+        graphViewSleep = (GraphView) currentView.findViewById(R.id.graph);
+
+        return currentView;
     }
 
 }
