c2e086eb2700cee12d1ef73dc76c57aed25024ed
Artyom Svetashov <arsvetashov@gmail.com>
2017-05-14 23:16:06 +0300

    removed GraphView library

--- app/src/main/java/com/example/a2/timetotrain/SleepFragment.java
+++ app/src/main/java/com/example/a2/timetotrain/SleepFragment.java
@@ -11,14 +11,13 @@ import com.jjoe64.graphview.GraphView;
 
 public class SleepFragment extends Fragment {
 
-    private GraphView graphViewSleep;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View currentView = inflater.inflate(R.layout.fragment_sleep, container, false);
-        graphViewSleep = (GraphView) currentView.findViewById(R.id.graph);
 
         return currentView;
     }
