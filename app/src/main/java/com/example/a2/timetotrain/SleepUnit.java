d8412966825b8cbb478d116e98a353997fe93392
Artyom Svetashov <arsvetashov@gmail.com>
2017-05-19 14:17:44 +0300

    Class SleepUnit

--- app/src/main/java/com/example/a2/timetotrain/SleepUnit.java
+++ app/src/main/java/com/example/a2/timetotrain/SleepUnit.java
@@ -4,40 +4,24 @@ package com.example.a2.timetotrain;
 import java.util.GregorianCalendar;
 
 public class SleepUnit {
-    final String[] monthNames = {"Янв", "Фев", "Мар", "Апр", "Май", "Июн", "Июл", "Авг", "Сен", "Окт", "Ноя", "Дек"};
+    private final String[] monthNames = {"Янв", "Фев", "Мар", "Апр", "Май", "Июн", "Июл", "Авг", "Сен", "Окт", "Ноя", "Дек"};
     private int id;     // Day of this year + year + hourOfEndSleeping
     private GregorianCalendar dateStartOfSleep, dateEndOfSleep;
-    private int day, year;   // from dateEndOfSleep
     private String month;   // month name
     private int rate;   // rating of sleep
     private String description;
     private int hoursOfSleeping, minutesOfSleeping, calories;
-
 
     public SleepUnit(GregorianCalendar dateStartOfSleep, GregorianCalendar dateEndOfSleep, int rate, String description) {
         this.dateStartOfSleep = dateStartOfSleep;
         this.dateEndOfSleep = dateEndOfSleep;
         this.rate = rate;
         this.description = description;
-
-        this.hoursOfSleeping = (int) ((dateEndOfSleep.getTimeInMillis() - dateEndOfSleep.getTimeInMillis()) / 3600000);
-        this.minutesOfSleeping = (int) ((dateEndOfSleep.getTimeInMillis() - dateEndOfSleep.getTimeInMillis()) / 60000);
-        this.day = dateEndOfSleep.DAY_OF_MONTH;
-        this.year = dateEndOfSleep.YEAR;
+        this.id = dateEndOfSleep.YEAR + dateEndOfSleep.DAY_OF_YEAR;
         this.month = monthNames[dateEndOfSleep.MONTH];
-        this.calories = (int) (this.hoursOfSleeping * 70.25 + this.minutesOfSleeping * 1.2);
-        this.id = dateEndOfSleep.DAY_OF_YEAR + this.year + dateEndOfSleep.HOUR_OF_DAY;
-    }
-
-
-    // Getters and Setters
-
-    public int getCalories() {
-        return calories;
-    }
-
-    public String getMonth() {
-        return month;
+        this.hoursOfSleeping = (int) ((dateEndOfSleep.getTimeInMillis() - dateStartOfSleep.getTimeInMillis()) / 3600000);
+        this.minutesOfSleeping = (int) ((dateEndOfSleep.getTimeInMillis() - dateStartOfSleep.getTimeInMillis()) / 60000);
+        this.calories = (int) (hoursOfSleeping * 70.25);
     }
 
     public int getId() {
@@ -48,40 +32,20 @@ public class SleepUnit {
         return dateStartOfSleep;
     }
 
-    public void setDateStartOfSleep(GregorianCalendar dateStartOfSleep) {
-        this.dateStartOfSleep = dateStartOfSleep;
-    }
-
     public GregorianCalendar getDateEndOfSleep() {
         return dateEndOfSleep;
     }
 
-    public void setDateEndOfSleep(GregorianCalendar dateEndOfSleep) {
-        this.dateEndOfSleep = dateEndOfSleep;
-    }
-
-    public int getDay() {
-        return day;
-    }
-
-    public int getYear() {
-        return year;
+    public String getMonth() {
+        return month;
     }
 
     public int getRate() {
         return rate;
     }
 
-    public void setRate(int rate) {
-        this.rate = rate;
-    }
-
     public String getDescription() {
         return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
     }
 
     public int getHoursOfSleeping() {
@@ -91,5 +55,10 @@ public class SleepUnit {
     public int getMinutesOfSleeping() {
         return minutesOfSleeping;
     }
+
+    public int getCalories() {
+        return calories;
+    }
+
 
 }
